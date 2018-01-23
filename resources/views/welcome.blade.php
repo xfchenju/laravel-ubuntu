7f9c786d7c5f2b2811352ed138a1b1ced9346624
xfchenju <975982556@qq.com>
2018-01-23 15:47:40 +0800

    modify index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello1 World1
                 </div>
 
                 <div class="links">
